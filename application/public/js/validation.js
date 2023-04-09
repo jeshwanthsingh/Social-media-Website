3ac2fa0da8c9d738dfad38bb8b871a80bf1baa6f
jeshwanthsingh <104602113+jeshwanthsingh@users.noreply.github.com>
2023-04-09 13:17:35 -0700

    added functions to show and hide error messages

--- application/public/js/validation.js
+++ application/public/js/validation.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const registrationForm = document.getElementById("registration-form");
-  
-registrationForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+
+    function showErrorMessage(id, message) {
+        const errorElement = document.getElementById(id);
+        errorElement.textContent = message;
+        errorElement.style.display = 'inline';
+    }
+
+    function hideErrorMessage(id) {
+        const errorElement = document.getElementById(id);
+        errorElement.style.display = 'none';
+    }
+
+    registrationForm.addEventListener("submit", (event) => {
+        event.preventDefault();
 
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
@@ -16,53 +27,54 @@ registrationForm.addEventListener("submit", (event) => {
         // Validate username
         const usernameRegex = /^[a-zA-Z][a-zA-Z0-9]{2,}$/;
         if (!usernameRegex.test(username)) {
-          isValid = false;
-          console.log("Username is invalid");
-          // Show error message
+            isValid = false;
+            showErrorMessage('username-error', 'Invalid username');
+        } else {
+            hideErrorMessage('username-error');
         }
-    
+
         // Validate password
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[/*\-+!@#$^&~[\]])[A-Za-z\d/*\-+!@#$^&~[\]]{8,}$/;
         if (!passwordRegex.test(password)) {
-          isValid = false;
-          console.log("Password is invalid");
-          // Show error message
+            isValid = false;
+            showErrorMessage('password-error', 'Invalid password');
+        } else {
+            hideErrorMessage('password-error');
         }
 
         // Validate confirm password
-    if (password !== confirmPassword) {
-        isValid = false;
-        console.log("Confirm password does not match password");
-        // Show error message
-      }
-  
-      // Validate age confirmation
-      if (!ageConfirmation) {
-        isValid = false;
-        console.log("User is not 13+ years old");
-        // Show error message
-      }
-  
-      // Validate TOS confirmation
-      if (!tosConfirmation) {
-        isValid = false;
-        console.log("User did not accept TOS and Privacy rules");
-        // Show error message
-      }
-  
-      if (isValid) {
-        console.log("Form is valid");
-        // Submit the form or show a success message
-        alert("Registration form submitted successfully!");
-        registrationForm.reset();
-      } else {
-        console.log("Form is invalid");
-        // Show a generic error message
-        alert("Please correct the errors in the form.");
-    }
- });
+        if (password !== confirmPassword) {
+            isValid = false;
+            showErrorMessage('confirm-password-error', 'Confirm password does not match password');
+        } else {
+            hideErrorMessage('confirm-password-error');
+        }
+
+        // Validate age confirmation
+        if (!ageConfirmation) {
+            isValid = false;
+            showErrorMessage('age-confirmation-error', 'User is not 13+ years old');
+        } else {
+            hideErrorMessage('age-confirmation-error');
+        }
+
+        // Validate TOS confirmation
+        if (!tosConfirmation) {
+            isValid = false;
+            showErrorMessage('tos-confirmation-error', 'User did not accept TOS and Privacy rules');
+        } else {
+            hideErrorMessage('tos-confirmation-error');
+        }
+
+        if (isValid) {
+            console.log("Form is valid");
+            // Submit the form or show a success message
+            alert("Registration form submitted successfully!");
+            registrationForm.reset();
+        } else {
+            console.log("Form is invalid");
+            // Show a generic error message
+            alert("Please correct the errors in the form.");
+        }
+    });
 });
-  
-  
-  
-  
