16056faf993ecc877f53c7eb43547e50eba8a8d9
jeshwanthsingh <104602113+jeshwanthsingh@users.noreply.github.com>
2023-04-19 19:08:32 -0700

    Added router for login page

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Jeshwanth Singh" });
 });
 
+router.get("/login", function(req,res){
+  res.render('login');
+  })
+  
 module.exports = router;
