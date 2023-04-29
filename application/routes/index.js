1f5a4f15a19389cf4e5b9b095bc8f4007f39329f
jeshwanthsingh <104602113+jeshwanthsingh@users.noreply.github.com>
2023-04-29 09:41:20 -0700

    added index in  the javascript

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Jeshwanth Singh" });
-});
+  res.render('index');
+})
 
 router.get("/login", function(req,res){
   res.render('login');
