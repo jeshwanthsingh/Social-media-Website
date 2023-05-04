4c3c16f97cb064adefaa19404b212902216fad22
jeshwanthsingh <104602113+jeshwanthsingh@users.noreply.github.com>
2023-05-04 13:30:02 -0700

    changed the index link

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/index", function(req,res){
-  res.render('index',{ title: 'Index'});
-  })
+router.get('/', function(req,res){
+  res.render('index',{ title: 'Home'});
+});
 
 router.get("/login", function(req,res) {
     res.render('login',{ title: 'Login'});
@@ -12,21 +12,21 @@ router.get("/login", function(req,res) {
 
 router.get("/postvideo", function(req,res){
   res.render('postvideo',{ title: 'Postvideo'});
-  })
+});
 
 router.get("/registration", function(req,res){
   res.render('registration',{ title: 'Register'});
-  })
+});
 
 router.get("/profile", function(req,res){
   res.render('profile',{ title: 'Profile'});
-  })
+});
 
 router.get("/viewpost", function(req,res){
   res.render('viewpost',{ title: 'Viewpost'});
-  })  
+});
 
-router.get('/images/CSC317.jpeg', function(req, res, next) {
+router.get('/images/CSC317.jpeg', function(req, res) {
   res.sendFile('CSC317.jpeg', { root: './public/images' });
 });
 
